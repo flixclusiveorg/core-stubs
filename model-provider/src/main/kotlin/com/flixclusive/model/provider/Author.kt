db9d4fc67ff381d46f9c1d93bb1c6a11f49b9504
rhenwinch <rhenwinchester@gmail.com>
2024-09-16 11:43:00 +0800

    feat(model-provider): add image property for Author class

--- model-provider/src/main/kotlin/com/flixclusive/model/provider/Author.kt
+++ model-provider/src/main/kotlin/com/flixclusive/model/provider/Author.kt
@@ -6,10 +6,12 @@ import kotlinx.serialization.Serializable
  * Represents an author entity with associated information such as name, github link, discords, and more.
  *
  * @property name The name of the author.
+ * @property image The optional image URL associated with the author's profile.
  * @property socialLink The optional social link associated with the author's profile.
  */
 @Serializable
 data class Author(
     val name: String,
+    val image: String? = null,
     val socialLink: String? = null,
 )
