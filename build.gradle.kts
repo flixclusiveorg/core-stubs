22f588dd640f8ee4fb0078d5e2cc0387cf685404
rhenwinch <rhenwinchester@gmail.com>
2025-01-05 02:08:12 +0800

    build(deps): upgrade AGP, Kotlin and other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,10 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.dokka)
 }
 
 tasks.dokkaHtmlMultiModule {
