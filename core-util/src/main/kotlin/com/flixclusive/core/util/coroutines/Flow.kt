2902805008b7447661f41d983cae4b80a1932801
rhenwinch <rhenwinchester@gmail.com>
2025-01-06 21:50:57 +0800

    feat(core-util): add `firstNotNull()` helper function

--- core-util/src/main/kotlin/com/flixclusive/core/util/coroutines/Flow.kt
+++ core-util/src/main/kotlin/com/flixclusive/core/util/coroutines/Flow.kt
@@ -1,7 +1,10 @@
 package com.flixclusive.core.util.coroutines
 
+import com.flixclusive.core.util.exception.safeCall
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
@@ -15,6 +18,12 @@ import kotlinx.coroutines.withContext
  * This should be used cautiously, as it blocks the thread.
  */
 fun <T> Flow<T>.blockFirst(): T = runBlocking { first() }
+
+/**
+ * Blocks the current thread until the first non-null value is emitted by the flow.
+ * This should be used cautiously, as it blocks the thread.
+ */
+fun <T> Flow<T>.blockFirstNotNull(): T = runBlocking { firstNotNull() }
 
 /**
  * Maps the flow elements using the given suspendable transformation function.
@@ -37,3 +46,54 @@ fun <T> Flow<T>.asStateFlow(
     started = started,
     initialValue = initialValue
 )
+
+/**
+ * The terminal operator that returns the first non-null element emitted by the flow and then cancels flow's collection.
+ * Throws [NoSuchElementException] if the flow was empty.
+ */
+suspend fun <T> Flow<T?>.firstNotNull(): T {
+    var result: Any? = null
+    collectWhile {
+        if (it != null) {
+            result = it
+            false
+        }
+
+        true
+    }
+
+    return result as T
+}
+
+suspend inline fun <T> Flow<T>.collectWhile(crossinline predicate: suspend (value: T) -> Boolean) {
+    val collector = object : FlowCollector<T> {
+        override suspend fun emit(value: T) {
+            // Note: we are checking predicate first, then throw. If the predicate does suspend (calls emit, for example)
+            // the the resulting code is never tail-suspending and produces a state-machine
+            if (!predicate(value)) {
+                throw AbortFlowException(this)
+            }
+        }
+    }
+
+    try {
+        collect(collector)
+    } catch (e: AbortFlowException) {
+        if (e.owner !== collector) throw e
+    }
+}
+
+class AbortFlowException(
+    @JvmField @Transient actual val owner: FlowCollector<*>
+) : CancellationException("Flow was aborted, no more elements needed") {
+
+    override fun fillInStackTrace(): Throwable {
+        safeCall {
+            return super.fillInStackTrace()
+        }
+
+        // Prevent Android <= 6.0 bug, #1866
+        stackTrace = emptyArray()
+        return this
+    }
+}
